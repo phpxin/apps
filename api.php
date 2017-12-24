ebd91641a7831e30e12ee6e6c1736be060ac8560
phpxin <=>
2017-12-24 21:20:36 +0800

    #

--- api.php
+++ api.php
@@ -1,12 +1,9 @@
 <?php
 header("Content-Type: application/json") ;
-include("config.php");
-$db = new PDO("mysql:host=127.0.0.1; dbname=test", DB_USER, DB_PWD) ;
+include_once 'dboper.class.php' ;
 
-$db->query("set names utf8");
+$dba = dboper::inst("testt") ;
+$list = $dba->select("select * from demo");
 
-$stat = $db->query("select * from demo") ;
-
-$rows = $stat->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($rows) ;
+echo json_encode($list) ;
 exit();
---
+++ dboper.class.php
@@ -0,0 +1,54 @@
+<?php
+include_once "./config.php" ;
+class dboper{
+
+    private static $_inst = [] ;
+    private $db ;
+
+    public static function inst($dbname){
+        if(!isset(self::$_inst[$dbname])|| !self::$_inst[$dbname] || !(self::$_inst[$dbname] instanceof self)){
+            self::$_inst[$dbname] = new self($dbname) ;
+        }
+
+        return self::$_inst[$dbname] ;
+    }
+
+    private function __construct($dbname){
+        $this->db = new PDO("mysql:host=127.0.0.1; dbname={$dbname}", DB_USER, DB_PWD) ;
+        $this->db->query("set names utf8");
+    }
+
+    private function __clone(){}
+
+    public function add($sql, $params=[]){
+        $statement = $this->db->prepare($sql) ;
+        if(!$statement->execute($params)){
+            return false;
+        }
+        return $this->db->lastInsertId();
+    }
+
+    public function delete($sql, $params=[]){
+        $statement = $this->db->prepare($sql) ;
+        if(!$statement->execute($params)){
+            return false;
+        }
+        return $statement->rowCount();
+    }
+
+    public function update($sql, $params=[]){
+        $statement = $this->db->prepare($sql) ;
+        if(!$statement->execute($params)){
+            return false;
+        }
+        return $statement->rowCount();
+    }
+
+    public function select($sql, $params=[]){
+        $statement = $this->db->prepare($sql) ;
+        if(!$statement->execute($params)){
+            return false;
+        }
+        return $statement->fetchAll();
+    }
+}
