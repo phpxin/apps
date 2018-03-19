1f157c0aa0b1d003a2b375e65c1f307f94b9d13f
lixin65535@126.com <lixin65535@126.com>
2018-03-19 11:06:17 +0800

    #

---
+++ ip.php
@@ -0,0 +1,50 @@
+<?php
+$realIp = realIp();
+var_dump($realIp) ;
+$ip = @file_get_contents("http://ip.taobao.com/service/getIpInfo.php?ip=".$realIp);
+$ip = json_decode($ip,true);
+
+
+function realIp() {
+	static $realip = NULL;
+	if ($realip !== NULL) {
+		return $realip;
+	}
+
+	if (isset($_SERVER)) {
+		if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+
+			//取X-Forwarded-for中第一个非unknown的有效Ip字符串
+			foreach ($arr as $ip) {
+				$ip = trim($ip);
+				if ($ip != 'unknown') {
+					$realip = $ip;
+					break;
+				}
+			}
+		} elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+			$realip = $_SERVER['HTTP_CLIENT_IP'];
+		} else {
+			if (isset($_SERVER['REMOTE_ADDR'])) {
+				$realip = $_SERVER['REMOTE_ADDR'];
+			} else {
+				$realip = '0.0.0.0';
+			}
+		}
+	} else {
+		if (getenv('HTTP_X_FORWARDED_FOR')) {
+			$realip = getenv('HTTP_X_FORWARDED_FOR');
+		} elseif (getenv('HTTP_CLIENT_IP')) {
+			$realip = getenv('HTTP_CLIENT_IP');
+		} else {
+			$realip = getenv('REMOTE_ADDR');
+		}
+	}
+
+	$onlineip = null;
+	preg_match("/[\d\.]{7,15}/", $realip, $onlineip);
+	$realip = !empty($onlineip[0]) ? $onlineip[0] : '0.0.0.0';
+	return $realip;
+}
+?>
